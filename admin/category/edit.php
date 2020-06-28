51991b0c61227adc43cd7472faf4c08fd94e1ca9
Ari Putra <ariteknologi@gmail.com>
2020-06-28 09:46:42 +0700

    init file

---
+++ admin/category/edit.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library System</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/style.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Library System</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="/library_system/admin/">Beranda</a></li>
+                <li><a href="/library_system/admin/category">Kategori</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="">Nama</label>
+                <input type="text" name="name" id="" required class="form-control">
+            </div>
+            <a href="/library_system/admin/category" class="btn btn-default">Batal</a>
+            <button type="submit" class="btn btn-primary">Perbarui</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library System</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="login">
+        <h1>Selamat Datang</h1>
+        <form action="">
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="text" name="email" id="" required class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="password" name="password" id="" required class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary"> Masuk </button>
+        </form>
+    </div>
+</body>
+</html>
